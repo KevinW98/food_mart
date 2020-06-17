6abcf959122e187572548d256ce3a01b4f3da3fd
weiyifan <yifan.wei@alumni.ubc.ca>
2020-06-17 05:37:32 -0700

    test

--- src/categories/diaryPage.js
+++ src/categories/diaryPage.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function diaryPage(){
     return (
         <div>
-            <h1>diary & eggs</h1>
+            <h1>diary & eggs hahahahhaha</h1>
         </div>
     )
 };
